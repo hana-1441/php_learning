3f19399fea43bb487e811402f2ab43e1864de4d5
heena-tops <heena.tops@gmail.com>
2023-10-30 08:58:34 +0530

    merge other

--- Array/practice_2.php
+++ Array/practice_2.php
@@ -10,7 +10,7 @@ echo "Array start";
 
 echo "<br>=========Array given==========";
 
-// $arr1 = ["names"=>"Admin","age"=>19];
+// $arr1 = ["names"=>"Admin","age"=>19];                
 // $arr2 = ["name"=>"User","age"=>19];
 // $arr3 = ["name"=>"User1","age"=>19];
 
@@ -20,9 +20,9 @@ $a2 = array("e"=>"red","f"=>"orange","z"=>"pink");
 
 
 echo "<pre>";
-print_r(array_diff($a2,$a1));
-print_r(array_diff_key($a2,$a1));
-print_r(array_diff_assoc($a2,$a1));
+print_r(array_diff($a2,$a1)); //compare based on value 
+print_r(array_diff_key($a2,$a1)); //compare based on key
+print_r(array_diff_assoc($a1,$a2)); // compare based on key and value both
 echo "</pre>";
 
 ?>
@@ -32,12 +32,12 @@ echo "</pre>";
 echo "=========================================================================";
 
 echo "<pre>";
-$a = array("volvo","ferrari","RR","Mercedeas","BMW","Audi");
+$a = array("volvo","ferrari","RR","Mercedeas","BMW","Audi","Bugatti");
 // echo "<pre>";
 print_r($a);
 
 print_r(array_chunk($a,2));
-print_r(array_slice($a,2));
+print_r(array_slice($a,4));
 
 ?>
 
@@ -82,11 +82,17 @@ $a = array(
 
     foreach ($a as $key => $value) 
     {
-        $data[]=$value['name']; // Also Good Way
+        // $data[]=$value['name']; // Also Good Way
         echo $value['name']."<br>";  //Also the other way
     }
 
-    print_r($data);
+    // print_r($data);
+
+    // array_column(array,colummn_key,index_key)
+
+    // array : required -> can be any multi-dimentional array or array of objects
+    // column_key : required -> return value from int or string kind of key(work as value of returned array)
+    // index_key : optional -> (work as key of returned array) confused ! need to ask about column_key and index_key
 
     print_r(array_column($a,'name')); //Best way but for 2d array
     echo "</pre>";
@@ -138,7 +144,7 @@ $a = array(
 
 <?php
 
-echo "<br>===============================================================<br>";
+echo "<br>=================in array==============================================<br>";
 
 $a = array("volvo"=>"xc90","bmw"=>"x5","audi"=>"a8");
 
@@ -157,6 +163,7 @@ echo "<br>";
 print_r(array_search("a8",$a));
 echo "<br>";
 echo "<br>";
+var_export(array_search("a8",$a));
 
 
 
@@ -164,7 +171,7 @@ echo "<br>";
 
 <?php
 
-echo "<br>===============================================================<br>";
+echo "<br>=======================pop========================================<br>";
 
 $a = array("red","green","orange","white");
 print_r($a);
@@ -207,7 +214,8 @@ echo "<br>";
 
 $array = array("dog","cat","lion");
 
-echo(array_reduce($array,"abc"));
+echo(array_reduce($array,"abc")); // like map() in python
+//create a single value by applying funciton to each and every array element
 
 echo "<br>";
 echo "<br>";
@@ -225,6 +233,7 @@ $array = array("dog","cat","lion");
 
 
 $var=implode(" ",$array); //convert array to string
+$var=implode(" ",$a); //for associative array will consider value 
 
 echo $var."<br>";
 echo gettype($var);
@@ -234,10 +243,105 @@ echo "<br><br>===================explode =======================<br><br>";
 
 $var1="Hola Amigo. ~from Miguel";
 
-$varx=explode(" ","Hola Amigo. ~from Miguel",2); //convert string to array
+$varx=explode(" ","Hola Amigo. ~from Miguel",4); //convert string to array
+//split string into array elements -> split performs based on white space 
 
 echo "<pre>";
 print_r($varx);
 echo gettype($varx);
+
+?>
+
+<?php
+echo "<br><br>===================combine merge =======================<br><br>";
+
+
+$name = array("a"=>"Peter","z"=>"Thor","c"=>"Iron Man","q"=>"Captain");
+$age = array("a"=>21,"z"=>100,"o"=>2000,"c"=>55);
+
+//Length should be Equal
+$newarray = (array_combine($name,$age));
+print_r($newarray);
+echo "<br>";
+print_r(array_merge($name,$age)); // it's like dictionary update method in python(if key will same then update value with the other one)
+//It will overwrite the last value
+
+
+
+echo "<br>";
+//this declaration needs a lookout
+$a = array("test"=>10,20,30,40);
+$b = array("test"=>'a','b','c','d');
+
+echo "<br>";
+
+echo "array merge example";
+echo "<br>";
+echo "<br>";
+
+echo "<pre>";
+print_r(array_merge($a,$b));
+echo "array merge recursive";
+echo "<br>";
+print_r(array_merge_recursive($a,$b));
+echo "</pre>";
+?>
+
+<?php
+
+echo "<br><br>===================array intersect=======================<br><br>";
+
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("e"=>"red","f"=>"green","g"=>"blue","a");
+echo "<pre>";
+$result=array_intersect($a1,$a2);//only check based on value
+print_r($result);
+
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
+$a2=array("a"=>"red","b"=>"green","c"=>"Blue");
+
+$result=array_intersect_assoc($a1,$a2); // check based on value and key both
+print_r($result);
+
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue");
+$a2=array("a"=>"red","c"=>"blue","d"=>"pink");
+
+$result=array_intersect_key($a1,$a2);
+print_r($result);
+
+
+function myfunction($a,$b)
+{
+if ($a===$b)
+  {
+  return 0;
+  }
+  return ($a>$b)?1:-1;
+}
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue");
+$a2=array("d"=>"red","b"=>"green","e"=>"blue");
+
+$result=array_intersect_uassoc($a1,$a2,"myfunction");
+print_r($result);
+
+
+function myfunctionukeys($a,$b)
+{
+if ($a===$b)
+  {
+  return 0;
+  }
+  return ($a>$b)?1:-1;
+}
+
+$a1=array("a"=>"red","b"=>"green","c"=>"blue");
+$a2=array("a"=>"blue","b"=>"black","e"=>"blue");
+
+$result=array_intersect_ukey($a1,$a2,"myfunctionukeys");
+print_r($result);
 
 ?>
